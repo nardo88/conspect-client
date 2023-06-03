8241694ddb490b0ea84a6630c30bef71f62f27dd
nardo88 <nardo1988@mail.ru>
2023-06-03 14:41:51 +0300

    axios

--- src/hooks/axios.hook.ts
+++ src/hooks/axios.hook.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  // baseURL: 'https://conspect-api.vercel.app/api/v1',
-  baseURL: 'http://localhost:5000/api/v1',
+  baseURL: 'https://conspect-api.vercel.app/api/v1',
+  // baseURL: 'http://localhost:5000/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
